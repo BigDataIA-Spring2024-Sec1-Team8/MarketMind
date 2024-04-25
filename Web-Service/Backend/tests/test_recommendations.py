865cc1fad161edec9ea18783b85fcd8b2e706b44
Shivani Guglani <guglani.s@northeastern.edu>
2024-04-25 09:17:38 -0400

    tests_resources_prompts

---
+++ Web-Service/Backend/tests/test_recommendations.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+import sys
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+from main import app
+
+
+client = TestClient(app)
+
+def test_recommendations():
+    payload = {"user_id": 1}
+    response = client.get("/user-recommendations/",  params=payload)
+    assert len(response.json()['response']) == 3
+
+
